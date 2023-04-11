cda11551ef3c8ef27e8985e4392d8821bc120d3c
gabriel <gabriel.f.lazari@gmail.com>
2023-04-11 00:00:06 -0300

    menu - animation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,25 @@
-// import './Nav.scss';
+import { useRef } from 'react';
 import logo from '../../assets/logo.png';
 import knight from '../../assets/knight.png';
-
+import menuVideo from '../../assets/menuzin.mp4';
 
 const Nav = () => {
+    const elemento = useRef(null);
+    const eventMenu = () => {
+        const menu = elemento.current;
+        menu.classList.toggle('menu-open');
+    }
+
     return (
         <nav>
             <img src={logo} alt='logo'></img>
-            <a href='#.'><img src={knight} alt='Hollow Knight'></img></a>
+            <a href='#.' onClick={eventMenu}><img src={knight} alt='Hollow Knight'></img></a>
+
+            <div ref={elemento} className='menu-closed'>
+                <video autoPlay loop muted>
+                    <source src={menuVideo} type="video/mp4" />
+                </video>
+            </div>
         </nav>
     )
 }
