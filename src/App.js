629700f4b9e66269668691028dc378e52a8ccb1b
gabriel <gabriel.f.lazari@gmail.com>
2023-04-11 02:26:35 -0300

    loading

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Nav from './components/Nav';
 import Headers from './components/Header';
 import Main from './components/Main';
 import Trailer from './components/ComponentsMain/Trailer';
-
-// import { }
+import Loading from './components/Loading';
 
 function App() {
+
   return (
     <div className="App">
+      <Loading />
       <Nav />
       <Headers />
       <Main />
---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import skin1 from '../../assets/skin1.png';
+
+const Loading = () => {
+    return (
+        <div className="loading">
+            <img src={skin1} alt="loading..."></img>
+        </div>
+    )
+}
+
+export default Loading
